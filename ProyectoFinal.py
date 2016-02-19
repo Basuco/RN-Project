0bf7892479ce838ada0ff1e43c430d60310c017b
wbinnovation <info@wbinnova.com>
2016-02-19 13:19:46 -0430

    resultados

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -1,4 +1,7 @@
 # import numpy as np
+import plotly.plotly as py
+import plotly.graph_objs as go
+
 import math
 import random
 
@@ -18,7 +21,7 @@ def NeuralNetwork( NumberNodes, fileName, CjtoValidacion):
 	matriz = readFile(fileName,ValueX,ValueY,clases)
 	
 	first = True
-	numeroPrueba = 10000
+	numeroPrueba = 1000
 	PromedioError = 0
 
 	while  numeroPrueba > 0:
@@ -68,13 +71,132 @@ def NeuralNetwork( NumberNodes, fileName, CjtoValidacion):
 			if first:
 				resultados = CorridaInicio(x,y,BiasHidden,BiasFinal,CjtoValidacion)
 				error = calculoErrorPromedio(resultados)
-				print error
+				# print error
 				first = False
 			j = j+1
 
 		PromedioError = PromedioError/len(ValueX)
 		numeroPrueba = numeroPrueba - 1
 	return [ x , y , BiasHidden , BiasFinal]
+
+def NeuralNetworkIris( NumberNodes, fileName):
+	entrada = list()
+	clases = list()
+	Validacion = list()
+	clasesVal = list()
+	readFileIris(fileName,entrada, clases,25,Validacion,clasesVal)
+	first = True
+	numeroPrueba = 500
+	PromedioError = 0
+
+	while  numeroPrueba > 0:
+		j=0
+
+		while j < len(entrada):
+			if first:
+				BiasHidden = Node()
+				BiasFinal = Node()
+
+				BiasHidden.out = 1
+				BiasFinal.out = 1
+
+
+				final = Node()
+
+				BiasFinal.next = final
+				BiasFinal.weight.append(math.floor(random.uniform(10,50))/100)
+
+				Inputs = list()
+				for elem in entrada[0]:
+					Inputs.append(Node())
+
+				k = 0
+				listaNodos = list()
+				while k<NumberNodes:
+					for elem in Inputs:
+						elem.weight.append(math.floor(random.uniform(10,50))/100)
+					
+					BiasHidden.weight.append(math.floor(random.uniform(10,50))/100)
+
+					listaNodos.append(Node())
+					listaNodos[k].next = final
+					listaNodos[k].weight.append(math.floor(random.uniform(10,50))/100)
+					
+					k = k+1
+				
+				BiasHidden.next = listaNodos
+				for elem in Inputs:
+						elem.next = listaNodos
+
+			i = 0
+			while i < len(Inputs):
+				Inputs[i].out = entrada[j][i]
+				i = i+1
+			
+			temp = TrainingIris(Inputs,float(clases[j]), BiasHidden, BiasFinal)
+
+			if first:
+				resultados = CorridaIris(Inputs,BiasHidden, BiasFinal, Validacion,clasesVal)
+				error = calculoErrorPromedio(resultados)
+				print error
+			 	first = False
+			j = j+1
+
+
+		numeroPrueba = numeroPrueba - 1
+
+	resultados = CorridaIris(Inputs,BiasHidden, BiasFinal, Validacion,clasesVal)
+	error = calculoErrorPromedio(resultados)
+
+
+
+def TrainingIris(Inputs,ElemClass,BiasHidden, BiasFinal):
+
+	k = 0
+	while k < len(Inputs[0].next):
+		net = 0
+		for Node in Inputs:
+			net = net + Node.out*Node.weight[k]
+		net = net + BiasHidden.out*BiasHidden.weight[k] ### + BIAS
+		Inputs[0].next[k].out = 1/(1+math.exp(-net))
+		k=k+1
+
+
+	# Net for final node
+	net = 0
+	for node in Inputs[0].next:
+		net = net + node.out*node.weight[0]
+	net = net + BiasFinal.out*BiasFinal.weight[0]
+	final = Inputs[0].next[0].next
+
+	final.out = 1/(1+math.exp(-net))
+
+	
+	#final error calculation
+	error = final.out*(1-final.out)*(ElemClass-final.out)
+ 
+
+	#alter hidden nodes weight
+	k = 0
+	BiasFinal.weight[0] = BiasFinal.weight[0] + error*BiasFinal.out
+	while k < len(Inputs[0].next):
+		node = Inputs[0].next[k]
+
+		node.weight[0] = node.weight[0] + error*node.out
+		error2 = node.out*(1-node.out)*(node.weight[0]*error)
+
+		
+		#alter input nodes weight
+
+		BiasHidden.weight[k] = BiasHidden.weight[k] + error2*BiasHidden.out
+		for node in Inputs:
+			node.weight[k] = node.weight[k] + error2*node.out
+
+		k = k+1
+
+	return (ElemClass-final.out)
+
+
 
 def GenerarValidacion():
 	matriz = list()
@@ -162,10 +284,44 @@ def readFile(fileName,X,Y, Classes):
 					cambio = cambio + 1
 				i = i + 1
 
-
 			X.append(float(ValueX)/20)
 			Y.append(float(ValueY)/20)
 			Classes.append(int(clase))
+
+
+
+def CorridaIris(Inputs,BiasHidden, BiasFinal, Validacion,Results):
+	i = 0
+	resultados = list()
+	while i < len(Validacion):
+		j = 0
+		while j < len(Validacion[i]):
+			Inputs[j].out = Validacion[i][j]
+			j = j+1
+			
+
+		k = 0
+		while k < len(Inputs[0].next):
+			net = 0
+			for Node in Inputs:
+				net = net + Node.out*Node.weight[k]
+			net = net + BiasHidden.out*BiasHidden.weight[k] ### + BIAS
+			Inputs[0].next[k].out = 1/(1+math.exp(-net))
+			k=k+1
+		# Calculo el net para calcular el out del nodo final
+		net = 0
+		for node in Inputs[0].next:
+			net = net + node.out*node.weight[0]
+		net = net + BiasFinal.out*BiasFinal.weight[0]
+		final = Inputs[0].next[0].next
+	
+		final.out = 1/(1+math.exp(-net))
+
+		resultados.append([final.out,Results[i]])
+		
+		i = i + 1
+
+	return resultados	
 
 
 
@@ -193,7 +349,7 @@ def CorridaInicio(X,Y,BiasHidden, BiasFinal, Validacion):
 
 			final.out = 1/(1+math.exp(-net))
 
-			resultados.append([final.out,Validacion[i][j][2]])
+			resultados.append([X.out,Y.out,final.out,Validacion[i][j][2]])
 
 			j = j+1
 		i = i + 1
@@ -209,55 +365,42 @@ def calculoErrorPromedio(resultados):
 		x = x+1
 	promedio = promedio/len(resultados)
 	return promedio
+def readFileIris(fileName,entrada, Classes,numero,Validacion,clasesVal):
+	with open(fileName) as file:
+		num = 0
+		for line in file:
 
+			i = 0
+			k = 0
+			valor = ""
+			clase = ""
+			cambio = 0
+			lista = list()
+			while i < len(line):
+				if line[i] != ' ':
+					if cambio<4:
+						valor = valor + line[i]
+					else:
+						clase = clase + line[i]
+				else:
+					cambio = cambio + 1
+					lista.append(float(valor))
+					valor = ""
+				i = i + 1
+			if num < 30:
+				Classes.append(float(clase))
+				entrada.append(lista)
+			else:
+				clasesVal.append(float(clase))
+				Validacion.append(lista)
+			num = num +1
+			if num == 50:
+				num = 0
 
 CjtoValidacion = GenerarValidacion()
-
-print "--------------PRUEBA 1000--------------------"
 print ""
 print "2 Nodos"
-red = NeuralNetwork(2,"datos_P1_RN_EM2016_n1000.txt", CjtoValidacion)
+red = NeuralNetwork(2,"test2000.txt", CjtoValidacion)
 resultados = CorridaInicio(red[0],red[1],red[2],red[3],CjtoValidacion)
 error = calculoErrorPromedio(resultados)
 print error
-print "3 Nodos"
-red = NeuralNetwork(3,"datos_P1_RN_EM2016_n1000.txt")
-resultados = CorridaInicio(red[0],red[1],red[2],red[3],CjtoValidacion)
-error = calculoErrorPromedio(resultados)
-print error
-print "4 Nodos"
-red = NeuralNetwork(4,"datos_P1_RN_EM2016_n1000.txt")
-resultados = CorridaInicio(red[0],red[1],red[2],red[3],CjtoValidacion)
-error = calculoErrorPromedio(resultados)
-print error
-print "5 Nodos"
-red = NeuralNetwork(5,"datos_P1_RN_EM2016_n1000.txt")
-resultados = CorridaInicio(red[0],red[1],red[2],red[3],CjtoValidacion)
-error = calculoErrorPromedio(resultados)
-print error
-print "6 Nodos"
-red = NeuralNetwork(6,"datos_P1_RN_EM2016_n1000.txt")
-resultados = CorridaInicio(red[0],red[1],red[2],red[3],CjtoValidacion)
-error = calculoErrorPromedio(resultados)
-print error
-print "7 Nodos"
-red = NeuralNetwork(7,"datos_P1_RN_EM2016_n1000.txt")
-resultados = CorridaInicio(red[0],red[1],red[2],red[3],CjtoValidacion)
-error = calculoErrorPromedio(resultados)
-print error
-print "8 Nodos"
-red = NeuralNetwork(8,"datos_P1_RN_EM2016_n1000.txt")
-resultados = CorridaInicio(red[0],red[1],red[2],red[3],CjtoValidacion)
-error = calculoErrorPromedio(resultados)
-print error
-print "9 Nodos"
-red = NeuralNetwork(9,"datos_P1_RN_EM2016_n1000.txt")
-resultados = CorridaInicio(red[0],red[1],red[2],red[3],CjtoValidacion)
-error = calculoErrorPromedio(resultados)
-print error
-print "10 Nodos"
-red = NeuralNetwork(10,"datos_P1_RN_EM2016_n1000.txt")
-resultados = CorridaInicio(red[0],red[1],red[2],red[3],CjtoValidacion)
-error = calculoErrorPromedio(resultados)
-print error
-print ""
